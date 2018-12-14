828211c435a0333220870d0a4b7f68cb4c27b4c3
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2018-12-14 14:19:43 +0300

    setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+function Hello() {
+    let phrase = "Slider";
+    return(
+        <h1>Hello! Here will be a {phrase}.</h1>
+    )
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+
+
 serviceWorker.unregister();
