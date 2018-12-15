11eff043eb347fd72c7216573f188d5e02736551
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2018-12-15 22:40:45 +0300

    menu & header added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-
 import styled from 'styled-components';
 
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color:  rgba(36, 36, 36, 0.8);
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
 class App extends Component {
   render() {
     return (
-     <h1>Hello! </h1>
-    );
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header/>
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu/>
+          </Grid>
+        </MenuWrapper>
+        
+        
+        <Main/>
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import * as serviceWorker from './serviceWorker';
 
 
 function Hello() {
-    let phrase = "Slider";
+    
     return(
-        <h1>Hello! Here will be a {phrase}.</h1>
+        <h1>index</h1>
     )
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 serviceWorker.unregister();
